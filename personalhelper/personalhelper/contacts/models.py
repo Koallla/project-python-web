3bdc7efebdbf171dbfa61ac5c45e81561f720608
v.dunkan <v.dunkan@gmail.com>
2021-09-06 01:56:18 +0300

    Add DB connection and SFTP storage

--- personalhelper/personalhelper/contacts/models.py
+++ personalhelper/personalhelper/contacts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.urls import reverse
+from storages.backends.sftpstorage import SFTPStorage
+SFS = SFTPStorage()
 
-# Create your models here.
+
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    contact_photo = models.FileField(storage=SFS, null=True)
+
+    def get_absolute_url(self):
+        return reverse("model_detail", kwargs={"pk": self.pk})
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Phone(models.Model):
+    value = models.CharField(max_length=10)
+    contact = models.ForeignKey(
+        'Contact', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return self.value
--- personalhelper/personalhelper/personalhelper/settings.py
+++ personalhelper/personalhelper/personalhelper/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'contacts.apps.ContactsConfig'
+    'contacts.apps.ContactsConfig',
     'notes.apps.NotesConfig',
 ]
 
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'personalhelper.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'web-project',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '82.193.125.109',
+        'PORT': '5432',
     }
 }
 
@@ -125,3 +129,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+DEFAULT_FILE_STORAGE = 'storages.backends.sftpstorage.SFTPStorage'
+
+SFTP_STORAGE_HOST = '82.193.125.109'
+SFTP_STORAGE_ROOT = '/home/'
+SFTP_STORAGE_PARAMS = {'username': 'dev_users', 'password': 'dev_2021'}
+SFTP_STORAGE_INTERACTIVE = False
