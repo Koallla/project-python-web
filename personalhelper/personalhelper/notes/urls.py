2333bb692e9a718e15e6f3d118319fda5b059169
v.dunkan <v.dunkan@gmail.com>
2021-09-09 20:20:41 +0300

    .gitignore

--- personalhelper/personalhelper/notes/urls.py
+++ personalhelper/personalhelper/notes/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('notes/', views.NoteListView.as_view(), name='notes:notes'),
+    path('all_notes/', views.NoteListView.as_view(), name='notes'),
     path('notes/<int:pk>', views.NoteDetailView.as_view(),
          name='note-detail'),
     path('tags/', views.TagListView.as_view(), name='tags'),
@@ -16,6 +16,6 @@ urlpatterns += [
     path('notes/create/', views.NoteCreateView.as_view(), name='note-create'),
     path('notes/update/<int:pk>/', views.NoteUpdate.as_view(),
          name='note-update'),
-    path('notes/<int:pk>/delete/', views.NoteDelete.as_view(),
+    path('notes/delete/<int:pk>/', views.NoteDelete.as_view(),
          name='note-delete'),
 ]
