bd4f7ac657d8b85ef1ae65d44743a99e202464a6
Mihail <41691809+Koallla@users.noreply.github.com>
2021-09-10 20:50:59 +0300

    added soup

--- personalhelper/personalhelper/hash.py
+++
@@ -1,5 +0,0 @@
-import hashlib
-import pickle
-
-s = hashlib.md5(pickle.dumps({'a':1})).hexdigest()
-print(s)
--- personalhelper/personalhelper/scraper/views.py
+++ personalhelper/personalhelper/scraper/views.py
@@ -1,32 +1,10 @@
-from django.shortcuts import render 
+from django.shortcuts import render
 from .models import Comand
-from crawling.crawling.spiders.football_spyder import FootballSpider
+from .soup import add_commands_to_db
 
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-from twisted.internet import reactor
-
-
-#   def run_crawl():
-#     process = CrawlerProcess(get_project_settings())
-#     process.crawl('football_spyder')
-#     await process.start()
-
-
-# def crawl_koovs():
-#     settings = get_project_settings()
-#     crawler = CrawlerProcess(settings)
-#     spider = FootballSpider()
-#     crawler.signals.connect(reactor.stop, signal=signals.spider_closed)
-#     crawler.configure()
-#     crawler.crawl(spider)
-#     crawler.start()
-#     reactor.run(installSignalHandlers=False)
 
 def index(request):
-
-    # crawl_koovs()
+    add_commands_to_db()
 
     data = Comand.objects.all()
-
     return render(request, 'comands-table.html', {'comands': data})
